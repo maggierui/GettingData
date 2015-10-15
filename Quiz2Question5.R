01d4fa58a8f7397dee0755c1a17cdc3c0d2bbbb1
Rui Hu <maggiehu.china@gmail.com>
2015-10-14 21:19:41 -0400

    quiz 3 question 5 R code

---
+++ Quiz2Question5.R
@@ -0,0 +1,10 @@
+df <- read.fwf(
+  file=url("https://d396qusza40orc.cloudfront.net/getdata%2Fwksst8110.for"),
+  widths=c(-1,9,-5,4,4,-5,4,4,-5,4,4,-5,4,4),
+  skip=4
+)
+## or using the new package readr
+x <- read_fwf(
+  file=url("https://d396qusza40orc.cloudfront.net/getdata%2Fwksst8110.for"),
+  skip=4,
+  fwf_widths(c(12, 7,4, 9,4, 9,4, 9,4)))
